f9bc75ac3f8e608a339a60021d43a23f42f46e4c
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-08-26 18:16:13 +0300

    imp

---
+++ index.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+
+        tabs.forEach(tab => {
+            tab.classList.remove('tab-active');
+        });
+
+        tab.classList.add('tab-active');
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active');
+        });
+
+        target.classList.add('active');
+    })
+})

e891b0ff2f1b347b4165ae7188b8cd1bcf573a18
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-09-01 13:11:50 +0300

    imp

--- script.js
+++ script.js
@@ -1,4 +1,66 @@
 createRecComponent({
-    root: document.querySelector('.hero')
-})
+    root: document.querySelector('.hero'),
+    segmentifyLoad: async () => {
+        const response = await fetch('product-list.json');
+        const data = await response.json();
 
+        return data.responses[0][0].params;
+    } ,
+    recContent: (product) => {
+        return `
+        <div class="swiper-slide">
+            <div class="card">
+                <a class="card-link" title="${product.name}" href="${product.url}">
+                    <div class="card-img">
+                        <img loading="lazy" src="${product.image}" alt="${product.name}">
+                    </div>
+                    <div class="card-content">
+                        <div class="card-title">
+                            <span class="cut-text">${product.name}</span>
+                        </div>
+                        <div class="card-price">
+                            <span>${product.priceText}</span>
+                        </div>
+                        <div class="card-label">
+                            <span class="${product.params.shippingFee === 'FREE' ? '' : 'hidden'} product-label-free-shipping"><i class="fas fa-truck"></i>Ücretsiz Kargo</span>
+                        </div>
+                    </div>
+                </a>
+                <div class="card-basket-add">
+                    <button class="btn-blue" onclick="notification()">Sepete Ekle</button>
+                </div>
+            </div>
+        </div>
+        `
+    },
+    recSkeleton: (recData) => {
+        return `
+        <div class="main">
+            <div class="main-title">
+                <div class="wrapper">
+                    <h2>Sizin için seçtiklerimiz</h2>
+                </div>
+            </div>
+            <div class="main-content">
+                <div class="main-content-nav">
+                    <div class=""><!--wrapper-->
+                        <div class="tab-group cut-text-items">
+                            <!--Tabs-->
+                            ${recData.userCategories.map((category, index) => {
+                                let _category = category.split('>').pop();
+                                return `<div class="tab ${index === 0 ? 'tab-active' : ''}" data-tab data-tab-key="${category}" title="${_category}">${_category}</div>`
+                            }).join('')}
+                        </div>
+                    </div>
+                </div>
+                <div class="main-content-content">
+                    <!--Tab contents-->
+                    ${recData.userCategories.map((category, index) => {
+                        return `<div class="tab-content ${index === 0 ? 'tab-content-active' : ''}" data-tab-content data-tab-key-pair="${category}"></div>`
+                    }).join('')}
+                </div>
+            </div>
+        </div>
+        `
+    },
+})
--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
-const initTabs = (recData) => {
-    const tabs = document.querySelectorAll('[data-tab-target]');
+const initTabs = (recData, recContent) => {
+    const tabs = document.querySelectorAll('[data-tab]');
     const tabContents = document.querySelectorAll('[data-tab-content]');
     //Render first tabs content and initialize the swiper for it
-    renderRecContent(tabs[0].dataset.tabKey, recData);
+    renderRecContent(tabs[0].dataset.tabKey, recData, recContent);
     swiper();
 
     //Add event listener to every tab
@@ -17,33 +17,25 @@ const initTabs = (recData) => {
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('tab-content-active');
         });
-        document.querySelector(tab.dataset.tabTarget).classList.add('tab-content-active');
+        document.querySelector(`[data-tab-key-pair="${tab.dataset.tabKey}"]`).classList.add('tab-content-active');
 
         //Render cards and then initialize swiper
-        renderRecContent(tab.dataset.tabKey, recData);
+        renderRecContent(tab.dataset.tabKey, recData, recContent);
         swiper();
     }));
 };
 
-const segmentifyLoad = async () => {
-    const response = await fetch('product-list.json');
-    const data = await response.json();
-
-    return data.responses[0][0].params;
-}
-
-const createRecComponent = async ({root}) => {
+const createRecComponent = async ({root, segmentifyLoad, recContent, recSkeleton}) => {
     try {
         const recData = await segmentifyLoad();
-        console.log(recData)
-        renderRecSkeleton(root, recData);
-        initTabs(recData);
+        renderRecSkeleton(root, recData, recSkeleton);
+        initTabs(recData, recContent);
     } catch (err) {
         console.log('Some error occured while rendering segmentify widget: ' + err)
     }
 }
 
-const renderRecContent = (target, recData) => {
+const renderRecContent = (target, recData, recContent) => {
     let root = document.querySelector(`[data-tab-key-pair="${target}"]`);
 
     root.innerHTML = `
@@ -51,31 +43,7 @@ const renderRecContent = (target, recData) => {
         <div class="swiper-wrapper">
         
         ${recData.recommendedProducts[target].map((product) => {
-            return `
-            <div class="swiper-slide">
-                <div class="card">
-                    <a class="card-link" title="${product.name}" href="${product.url}">
-                        <div class="card-img">
-                            <img loading="lazy" src="${product.image}" alt="${product.name}">
-                        </div>
-                        <div class="card-content">
-                            <div class="card-title">
-                                <span class="cut-text">${product.name}</span>
-                            </div>
-                            <div class="card-price">
-                                <span>${product.priceText}</span>
-                            </div>
-                            <div class="card-label">
-                                <span class="${product.params.shippingFee === 'FREE' ? '' : 'hidden'} product-label-free-shipping"><i class="fas fa-truck"></i>Ücretsiz Kargo</span>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="card-basket-add">
-                        <button class="btn-blue" onclick="notification()">Sepete Ekle</button>
-                    </div>
-                </div>
-            </div>
-            `
+            return recContent(product);
         }).join('')}
         </div>
         <div class="swiper-button-prev"></div>
@@ -84,35 +52,8 @@ const renderRecContent = (target, recData) => {
     `
 }
 
-const renderRecSkeleton = (root, recData) => {
-    root.innerHTML = `
-    <div class="main">
-        <div class="main-title">
-            <div class="wrapper">
-                <h2>Sizin için seçtiklerimiz</h2>
-            </div>
-        </div>
-        <div class="main-content">
-            <div class="main-content-nav">
-                <div class=""><!--wrapper-->
-                    <div class="tab-group cut-text-items">
-                        <!--Tabs-->
-                        ${recData.userCategories.map((category, index) => {
-                            let _category = category.split('>').pop();
-                            return `<div class="tab ${index === 0 ? 'tab-active' : ''}" data-tab-target="#tab-${index}" data-tab-key="${category}" title="${_category}">${_category}</div>`
-                        }).join('')}
-                    </div>
-                </div>
-            </div>
-            <div class="main-content-content">
-                <!--Tab contents-->
-                ${recData.userCategories.map((category, index) => {
-                    return `<div id="tab-${index}" class="tab-content ${index === 0 ? 'tab-content-active' : ''}" data-tab-content data-tab-key-pair="${category}"></div>`
-                }).join('')}
-            </div>
-        </div>
-    </div>
-    `
+const renderRecSkeleton = (root, recData, recSkeleton) => {
+    root.innerHTML = recSkeleton(recData);
 }
 
 const swiper = () => {
@@ -127,11 +68,9 @@ const swiper = () => {
 }
 
 const notification = () => {
-    var x = document.querySelector("#toast");
-
-    x.className = "show";
-
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    let notification = document.querySelector("#toast");
+    notification.className = "show";
+    setTimeout(function(){ notification.className = notification.className.replace("show", ""); }, 3000);
 }
 
 
